b994c9f4567d5d22de2478a992d39f027fa4bb91
Martin Lea <you@example.com>
2021-07-22 15:35:01 +0100

    tddft_input_gen made, short script to create .inp files for TDDFT in ORCA. use through slurm, the ORCA-ASE version coming soon

---
+++ abs_spectra/tddft_input_gen.py
@@ -0,0 +1,10 @@
+
+import sys
+def tddft_input_gen(functional, basis, roots, xyz, procs):
+    inp = open('./tddft_calc.inp', 'w+')
+    inp.write('! RKS '+str(functional)+' '+str(basis)+' 'str(basis)+'/c def2/J RIJCOSX \n'
+              '%pal nprocs '+str(procs)+' end \n'
+              '%tddft nroots '+str(roots)+' maxdim 10 end \n'
+              '* xyzfile 0 1 '+str(xyz)+' \n')
+    inp.close()
+tddft_input_gen(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
